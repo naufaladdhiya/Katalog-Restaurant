f8af7262408a3ca58548ac583bb20741dfec6d91
naufaladdhiya <naufal.addhiya@gmail>
2023-04-09 21:38:33 +0700

    refactor datasource to RestaurantDbSource

---
+++ src/scripts/data/restaurantdb-source.js
@@ -0,0 +1,21 @@
+import API_ENDPOINT from '../globals/api-endpoint';
+
+class RestaurantSource {
+  static async restaurantList() {
+    const response = await fetch(API_ENDPOINT.RESTAURANTS);
+    const responseJSON = await response.json();
+    return responseJSON.restaurants;
+  }
+
+  static async restaurantDetail(id) {
+    const response = await fetch(API_ENDPOINT.DETAIL(id));
+    return response.json();
+  }
+
+  static async postReview(id) {
+    const response = await fetch(API_ENDPOINT.DETAIL(id));
+    return response.json();
+  }
+}
+
+export default RestaurantSource;
