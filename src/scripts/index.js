421040fa6b1f7ed516c4acbea63a6b5d6641adcc
naufaladdhiya <naufal.addhiya@gmail>
2023-04-09 21:42:07 +0700

    adding simple routes && other

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,57 +1,19 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
-import DATA from '../DATA.json';
+import App from './views/app';
 
-const hamburger = document.querySelector('#menu');
-const main = document.querySelector('main');
-const drawer = document.querySelector('#navmenu');
-const hero = document.querySelector('#hero');
-const nav = document.getElementById('navcontainer');
-
-hamburger.addEventListener('click', (event) => {
-  if (drawer.style.maxHeight) {
-    drawer.style.maxHeight = null;
-  } else {
-    drawer.style.maxHeight = `${drawer.scrollHeight}px`;
-  }
-  event.stopPropagation();
+const app = new App({
+  button: document.querySelector('#menu'),
+  drawer: document.querySelector('#navmenu'),
+  content: document.querySelector('#mainContent'),
 });
 
-main.addEventListener('click', (event) => {
-  drawer.style.maxHeight = null;
-  event.stopPropagation();
+window.addEventListener('hashchange', () => {
+  app.renderPage();
 });
 
-nav.addEventListener('click', (event) => {
-  drawer.style.maxHeight = null;
-  event.stopPropagation();
+window.addEventListener('load', () => {
+  app.renderPage();
+  // swRegister();
 });
-
-hero.addEventListener('click', (event) => {
-  drawer.style.maxHeight = null;
-  event.stopPropagation();
-});
-
-const getExploreRestaurant = (data) => {
-  data.restaurants.forEach((restaurant) => {
-    const restaurantItem = document.getElementById('main-resto_list');
-    restaurantItem.innerHTML += `
-        
-        <article tabindex="0" class="resto-item">
-            <a href="#">
-                <div class="resto-item_content">
-                    <img class="resto-item_image" src="${restaurant.pictureId}" alt="Gambar ${restaurant.name}" tabindex="0"/>
-                    <p tabindex="0" class="resto-item_city" alt="kota restoran">${restaurant.city}<span class="resto-item_rating" 
-                        aria-label="rating resto ${restaurant.rating}">&star; ${restaurant.rating}</span>
-                    </p>
-                    <p tabindex="0" class="resto-item_name" alt="nama restoran">${restaurant.name}</p>
-                    <p tabindex="0" class="resto-item_desc" alt="deskripsi restoran">${restaurant.description}</p>
-                </div>
-            </a>
-        </article>
-        `;
-  });
-};
-
-getExploreRestaurant(DATA);
