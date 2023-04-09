767f45fbd200939b9b556353dec506a2de1228f4
naufaladdhiya <naufal.addhiya@gmail>
2023-04-09 21:39:17 +0700

    adding simple routes system

---
+++ src/scripts/routes/routes.js
@@ -0,0 +1,11 @@
+import Restaurant from '../views/pages/restaurant';
+import Detail from '../views/pages/detail';
+// import Favorite from '../views/pages/favorite';
+
+const routes = {
+  '/': Restaurant, // default page
+  //   '/favorite': Favorite,
+  '/detail/:id': Detail,
+};
+
+export default routes;
